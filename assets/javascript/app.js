33043377426fd976cd8b51dd6243031157a27737
Hammrfall <34660593+Hammrfall@users.noreply.github.com>
2018-03-12 15:59:51 -0400

    add animated gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,9 @@
 //Global variables
-var gamestate = 0;
+var gameState = 0;
 /* 0 - reset
    1 -guessing
    2-results */
+
 var guessedIndex = -1;
 var guessMade = false;
 var currentTrivia;
@@ -10,25 +11,9 @@ var currentTriviaIndex = -1;
 var wins = 0;
 var losses = 0;
 var unAnswered = 0;
-var triviaArray = []
 var gameTimeMax = 30;
 var gameTime = gameTimeMax;
 var GameCountDown;
-
-var trivia = function () { // triviaObject
-    this.correctIndex;
-    this.question = ""
-    this.choices = []
-    this.gifRef
-    isCorrect = function (index) {
-        var answer = false;
-        if (index === this.correctIndex) {
-            answer = true;
-        }
-        return answer;
-    }
-
-} //end of trivia object
 
 var gameTimer = setInterval(function () {
     GameCountDown--;
@@ -36,14 +21,12 @@ var gameTimer = setInterval(function () {
         var secondsString;
         if (GameCountDown >= 10) {
             secondsString = GameCountDown;
-        } else if (GameCountDown == 0) {
-            secondsString = "00";
         } else {
             secondsString = "0" + GameCountDown;
         }
         $("#timerdisplay").text("00:" + secondsString);
         if (GameCountDown == 0) {
-            checkAnswer();
+            checkAnswer()
         }
     }
 }, 1000);
@@ -63,49 +46,6 @@ function resetGame() {
     populatePage();
 }
 
-function populateTriviaArray() {
-    // populates the trivia array with trivia questions and answers
-
-    var firstQuestion = new trivia;
-    firstQuestion.question = "Einstein was ___  years old when he published his general theory of relativity";
-    var guess1 = "26"
-    firstQuestion.choices.push(guess1);
-    var guess2 = "28";
-    firstQuestion.choices.push(guess2);
-    var guess3 = "30";
-    firstQuestion.choices.push(guess3);
-    var guess4 = "32";
-    firstQuestion.choices.push(guess4);
-    firstQuestion.correctIndex = 0;
-    triviaArray.push(firstQuestion);
-
-    var secondQuestion = new trivia;
-    secondQuestion.question = "The eiffel tower is _____ tall";
-    var guess1 = "846 ft"
-    secondQuestion.choices.push(guess1);
-    var guess2 = "987 ft";
-    secondQuestion.choices.push(guess2);
-    var guess3 = "1,063 feet";
-    secondQuestion.choices.push(guess3);
-    var guess4 = "1,244 feet";
-    secondQuestion.choices.push(guess4);
-    secondQuestion.correctIndex = 2;
-    triviaArray.push(secondQuestion);
-
-    var thirdQuestion = new trivia;
-    thirdQuestion.question = "The statue of liberty was a gift from _____";
-    var guess1 = "Germany"
-    thirdQuestion.choices.push(guess1);
-    var guess2 = "England";
-    thirdQuestion.choices.push(guess2);
-    var guess3 = "Spain";
-    thirdQuestion.choices.push(guess3);
-    var guess4 = "France";
-    thirdQuestion.choices.push(guess4);
-    thirdQuestion.correctIndex = 3;
-    triviaArray.push(thirdQuestion);
-};
-
 function getNextTrivia() {
     currentTriviaIndex++;
     if (currentTriviaIndex < triviaArray.length) {
@@ -114,10 +54,14 @@ function getNextTrivia() {
         GameCountDown = gameTimeMax;
         $("#timerdisplay").text("00:" + GameCountDown);
         guessMade = false;
-        gameTimer.start; 
+        gameState = 1
+        gameTimer.start;
+        populatePage();
     }
     //ToDo:  create an else that calls an end of game function, and that function sets a timer to restart the game
-
+    else {
+        gameEnd()
+    }
 }
 
 function populatePage() {
@@ -133,7 +77,7 @@ function populatePage() {
 }
 
 $(".choice").click("click", function () {
-    if (guessedIndex < 0) {
+    if (guessedIndex < 0 && gameState === 1) {
         guessMade = true;
         guessedIndex = $(this).attr('value');
         $(this).css("background", "rgb(67, 176, 42");
@@ -142,7 +86,7 @@ $(".choice").click("click", function () {
 });
 
 function checkAnswer() {
-    gamestate = 2;
+    gameState = 2;
     if (guessedIndex == -1) {
         $("#message").text("You ran out of time");
         unAnswered++;
@@ -162,9 +106,19 @@ function checkAnswer() {
         $(rightID).css("background", "rgb(67, 176, 42");
         losses++;
     }
-    //ToDo: code to display gif
-    var newGameTimer = setTimeout(function () {
+    $("#gifholder").html('<img src="assets/images/' + currentTrivia.gifRef + '" alt="animatedGif" >');
+    
+    var newGameTimer = setTimeout(function (){        
+        $("#gifholder").html("")
         getNextTrivia();
-        populatePage();
     }, 3000);
+}
+
+function gameEnd() {
+    gameState = 3
+    $("#message").text("GAME OVER.  You guessed " + wins + " correctly; " + losses + " incorrectly and had " + unAnswered + " unanswered.");
+    var GameTimer = setTimeout(function () {
+        currentTriviaIndex = -1
+        resetGame()
+    }, 7000);
 }
