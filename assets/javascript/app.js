28efa7e67c8a268e88c5217e45825139a80b37ac
Hammrfall <34660593+Hammrfall@users.noreply.github.com>
2018-03-15 15:00:38 -0400

    finalize html css and js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 //Global variables
-/* The global variable TriviaArray,
+/* The global variable TriviaArray[],
 is declared in the data.js file.
 It is an array of trivia objects, 
 defined and populated in the data.js file */
@@ -19,55 +19,54 @@ var gameTimeMax = 30;
 var gameTime = gameTimeMax;
 var GameCountDown;
 
+$(document).ready(function () {
+    populateTriviaArray(); //calls function in the data.js file
+    resetGame();
+    getNextTrivia();
+    gameTimer.start;
+});
+
 var gameTimer = setInterval(function () {
     GameCountDown--;
     if (GameCountDown >= 0 && guessMade === false) {
         var secondsString;
         if (GameCountDown >= 10) {
             secondsString = GameCountDown;
-        } else {
+        } 
+        else {
             secondsString = "0" + GameCountDown;
         }
         $("#timerdisplay").text("00:" + secondsString);
         if (GameCountDown == 0) {
-            checkAnswer()
+            checkAnswer();
         };
     }
 }, 1000);
 
-$(document).ready(function () {
-    populateTriviaArray(); //calls function in the data.js file
-    resetGame();
-    gameTimer.start;
-});
-
 function resetGame() {
-    guessedIndex = -1;
-    currentTriviaIndex = -1;
     wins = 0;
     losses = 0;
     unAnswered = 0;
-    GameCountDown = gameTimeMax
-    getNextTrivia();
+    currentTriviaIndex = -1;
 }
 
 function getNextTrivia() {
     currentTriviaIndex++;
     if (currentTriviaIndex < triviaArray.length) {
+        gameState = 1;
         currentTrivia = triviaArray[currentTriviaIndex];
         guessedIndex = -1;
         GameCountDown = gameTimeMax;
         $("#timerdisplay").text("00:" + GameCountDown);
         guessMade = false;
-        gameState = 1
         populatePage();
     } else {
-        gameEnd()
+        gameEnd();
     }
 }
 
 function populatePage() {
-    $("#timerdisplay").text("00:" + gameTimeMax)
+    $("#timerdisplay").text("00:" + gameTimeMax);
     $("#question").text(currentTrivia.question);
     var idString;
     for (var i = 0; i < 4; i++) {
@@ -89,11 +88,12 @@ $(".choice").click("click", function () {
 
 function checkAnswer() {
     gameState = 2;
-    if (guessedIndex == -1) {
+    if (guessedIndex === -1) {
         $("#message").text("You ran out of time");
-        unAnswered++;
+
         var rightID = "#label" + (currentTrivia.correctIndex + 1);
         $(rightID).css("background", "rgb(67, 176, 42");
+        unAnswered++;
 
     } else if (guessedIndex == currentTrivia.correctIndex) {
         $("#message").text("That is correct!");
@@ -110,16 +110,17 @@ function checkAnswer() {
     }
     $("#gifholder").html('<img src="assets/images/' + currentTrivia.gifRef + '" alt="animatedGif" >');
     var newGameTimer = setTimeout(function () {
-        $("#gifholder").html("")
+        $("#gifholder").html("");
         getNextTrivia();
     }, 7000);
 }
 
 function gameEnd() {
-    gameState = 3
+    gameState = 3;
     $("#message").text("GAME OVER.  You guessed " + wins + " correctly; " + losses + " incorrectly and had " + unAnswered + " unanswered.");
     var GameTimer = setTimeout(function () {
-        currentTriviaIndex = -1
-        resetGame()
+        currentTriviaIndex = -1;
+        resetGame();
+        getNextTrivia();
     }, 10000);
 }
--- assets/javascript/data.js
+++ assets/javascript/data.js
@@ -5,53 +5,45 @@ var trivia = function () { // triviaObject
     this.question = ""
     this.choices = []
     this.gifRef = ""
-    isCorrect = function (index) {
+    isCorrect = function (guess) {
         var answer = false;
-        if (index === this.correctIndex) {
+        if (guess === this.correctIndex) {
             answer = true;
         }
         return answer;
     }
-} //end of trivia object
+}
 
-
-function populateTriviaArray() {
-    // populates the trivia array with trivia questions and answers
-
+function populateTriviaArray() { // populates the trivia array with trivia questions and answers
     var firstQuestion = new trivia;
     firstQuestion.question = "Einstein was ___  years old when he published his general theory of relativity";
     firstQuestion.choices.push("26", "28", "30", "32");
     firstQuestion.correctIndex = 0;
-    firstQuestion.gifRef = "Einstein.gif"
-    triviaArray.push(firstQuestion);
+    firstQuestion.gifRef = "Einstein.gif";
 
     var secondQuestion = new trivia;
     secondQuestion.question = "The eiffel tower is _____ tall";
-    secondQuestion.choices.push("846 ft", "987 ft", "1,063 feet", "1,244 feet");
+    secondQuestion.choices.push("846 feet", "987 feet", "1,063 feet", "1,244 feet");
     secondQuestion.correctIndex = 2;
-    secondQuestion.gifRef = "eiffel.gif"
-    triviaArray.push(secondQuestion);
-
+    secondQuestion.gifRef = "eiffel.gif";
 
     var thirdQuestion = new trivia;
     thirdQuestion.question = "The statue of liberty was a gift from _____";
     thirdQuestion.choices.push("Germany", "England", "Spain", "France");
     thirdQuestion.correctIndex = 3;
-    thirdQuestion.gifRef = "liberty.gif"
-    triviaArray.push(thirdQuestion);
+    thirdQuestion.gifRef = "liberty.gif";
 
     var fourthQuestion = new trivia;
-    fourthQuestion.question = "The only president confirmed to have a tattoo is ______"
-    fourthQuestion.choices.push("Andrew Jackson", "Ulysses S. Grant", "Theodore Roosevelt", "Barack Obama")
-    fourthQuestion.correctIndex = 2
-    fourthQuestion.gifRef = "teddy.gif"
-    triviaArray.push(fourthQuestion)
+    fourthQuestion.question = "The only president confirmed to have a tattoo is ______";
+    fourthQuestion.choices.push("Andrew Jackson", "Ulysses S. Grant", "Theodore Roosevelt", "Barack Obama");
+    fourthQuestion.correctIndex = 2;
+    fourthQuestion.gifRef = "teddy.gif";
 
     var fifthQuestion = new trivia
-    fifthQuestion.question = "Which of these took the longest to build?"
-    fifthQuestion.choices.push("Taj Mahal", "Buckingham Palace", "Brandenburg Gate", "Leaning Tower of Pisa")
-    fifthQuestion.correctIndex = 3
-    fifthQuestion.gifRef = "pisa.gif"
-    triviaArray.push(fifthQuestion)
+    fifthQuestion.question = "Which of these took the longest to build?";
+    fifthQuestion.choices.push("Taj Mahal", "Buckingham Palace", "Brandenburg Gate", "Leaning Tower of Pisa");
+    fifthQuestion.correctIndex = 3;
+    fifthQuestion.gifRef = "pisa.gif";
 
+    triviaArray.push(firstQuestion, secondQuestion, thirdQuestion, fourthQuestion, fifthQuestion);
 };
